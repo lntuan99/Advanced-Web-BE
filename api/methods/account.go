6f02d01fc118f51bcb80d58840b0700a58337363
annalla <danghongxuyen2000@gmail.com>
2021-11-15 18:43:39 +0700

    update register

--- api/methods/account.go
+++ api/methods/account.go
@@ -1,6 +1,11 @@
 package methods
 
 import (
+	"fmt"
+	"path/filepath"
+	"strconv"
+	"time"
+
 	api_jwt "advanced-web.hcmus/api/api-jwt"
 	"advanced-web.hcmus/api/base"
 	req_res "advanced-web.hcmus/api/req_res_struct"
@@ -12,6 +17,9 @@ import (
 
 func MethodRegisterAccount(c *gin.Context) (bool, string, interface{}) {
 	var registerAccountInfo req_res.PostRegisterAccount
+
+	_ = c.Request.ParseMultipartForm(20971520)
+
 	if err := c.ShouldBindJSON(&registerAccountInfo); err != nil {
 		return false, base.CodeBadRequest, nil
 	}
@@ -35,10 +43,61 @@ func MethodRegisterAccount(c *gin.Context) (bool, string, interface{}) {
 		return false, base.CodeRegisterAccountFail, nil
 	}
 
-	if err := model.DBInstance.Create(&newAccount).Error; err != nil {
-		return false, base.CodeRegisterAccountFail, nil
+	// if err := model.DBInstance.Create(&newAccount).Error; err != nil {
+	// 	return false, base.CodeRegisterAccountFail, nil
+	// }
+	gender := 0
+	if registerAccountInfo.Gender == "2" {
+		gender = model.GENDER_FEMALE
+	} else if registerAccountInfo.Gender == "1" {
+		gender = model.GENDER_MALE
+	} else {
+		gender = model.GENDER_UNKNOWN
 	}
 
+	var birthday time.Time
+	if registerAccountInfo.Birthday != "" {
+		tm, err := strconv.ParseInt(registerAccountInfo.Birthday, 10, 64)
+		if err != nil {
+			return false, base.CodeInvalidDate, nil
+		}
+		birthday = time.Unix(tm, 0)
+	}
+
+	if !util.IsEmailValid(registerAccountInfo.Email) {
+		return false, base.CodeInvalidEmailFormat, nil
+	}
+
+	var newUser = model.User{
+		Name:         registerAccountInfo.Name,
+		Code:         registerAccountInfo.Code,
+		Email:        registerAccountInfo.Email,
+		Phone:        registerAccountInfo.Phone,
+		Gender:       uint(gender),
+		Birthday:     birthday,
+		IdentityCard: registerAccountInfo.IdentityCard,
+		Enabled:      true,
+	}
+	if err := model.DBInstance.Create(&newUser).Error; err != nil {
+		return false, base.CodeRegisterAccountFail, nil
+	}
+	model.DBInstance.Model(&newAccount).Update("user_id", newUser.ID)
+
+	// FormFile returns the first file for the given key `avatar`
+	_, header, errFile := c.Request.FormFile("avatar")
+	if errFile == nil {
+		newFileName := fmt.Sprintf("%v%v", time.Now().Unix(), filepath.Ext(header.Filename))
+		folderDst := fmt.Sprintf("/system/users/%v", newUser.ID)
+
+		util.CreateFolder(folderDst)
+
+		fileDst := fmt.Sprintf("%v/%v", folderDst, newFileName)
+		if err := util.SaveUploadedFile(header, folderDst, newFileName); err == nil {
+			model.DBInstance.
+				Model(&newUser).
+				Update("avatar", fileDst)
+		}
+	}
 	return true, base.CodeSuccess, nil
 }
 
--- api/req_res_struct/request.go
+++ api/req_res_struct/request.go
@@ -4,6 +4,13 @@ type PostRegisterAccount struct {
 	Username       string `form:"username" json:"username"`
 	Password       string `form:"password" json:"password"`
 	RetypePassword string `form:"retypePassword" json:"retypePassword"`
+	Name           string `form:"name" json:"name"`
+	Code           string `form:"code" json:"code"`
+	Email          string `form:"email" json:"email"`
+	Phone          string `form:"phone" json:"phone"`
+	Birthday       string `form:"birthday" json:"birthday"`
+	Gender         string `form:"gender" json:"gender"`
+	IdentityCard   string `form:"identityCard" json:"identityCard"`
 }
 
 type PostLoginAccount struct {
